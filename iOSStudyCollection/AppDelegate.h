2f18cb48442291a0e62c8e53775f9de2b125f3a7
赵赫 <zhaohe@zhaohedemacbook-pro-2.local>
2018-03-05 14:17:22 +0800

    Initial Commit

---
+++ iOSStudyCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSStudyCollection
+//
+//  Created by 赵赫 on 2018/3/5.
+//  Copyright © 2018年 赵赫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
